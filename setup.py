b068c850ea4610bf3b2c1ab83b5a42de797f4a3c
sai <bejawadasaimahendra@gmail.com>
2025-06-10 16:39:43 +0530

    debugging bro igonre it ok?

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PY_TELUGU_VERSION",
-    version="1.1",
+    version="1.0.1",
     packages=find_packages(),
     include_package_data=True,
       package_data={
