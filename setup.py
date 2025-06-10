8e413762d14403f2b3f87d55bdb7e056521b69bc
sai <bejawadasaimahendra@gmail.com>
2025-06-10 16:30:07 +0530

    added description and json to setup file

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="PY_TELUGU_VERSION",
-    version="1.0",
+    version="1.1",
     packages=find_packages(),
     include_package_data=True,
+      package_data={
+        "PY_TELUGU_VERSION": ["keywords_te.json"],
+    },
     entry_points={
         'console_scripts': [
             'pythontel = PY_TELUGU_VERSION.auto_update:main',
@@ -13,7 +16,7 @@ setup(
     },
     author="sunstromium",
     description="literal telugu package for python programming",
-    long_description="-- — test version",
+    long_description="this package convert the english keywords in python to telugu , i am currently using json file to convert them, it is lot of pain to every single word so it takes times to complete this project ",
     long_description_content_type="text/plain",
     classifiers=[
         "Programming Language :: Python :: 3",
